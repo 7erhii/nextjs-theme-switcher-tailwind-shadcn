e831a9910ec91692bfe3b16967783dda34a4484c
7erhiy <7erhiy@gmail.com>
2024-09-03 01:00:48 -0300

    core

--- components/ThemeToggle/index.js
+++ components/ThemeToggle/index.js
@@ -1,23 +1,47 @@
 "use client";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Sun, Moon, Monitor } from "lucide-react";
+
 export default function ThemeToggle() {
-  function toggleTheme() {
+  const [theme, setTheme] = useState("system");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "system";
+    applyTheme(savedTheme);
+  }, []);
+
+  function applyTheme(theme) {
     const root = document.documentElement;
-    const isDark = root.classList.contains("dark");
-    if (isDark) {
+    if (theme === "dark") {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else if (theme === "light") {
       root.classList.remove("dark");
       localStorage.setItem("theme", "light");
     } else {
-      root.classList.add("dark");
-      localStorage.setItem("theme", "dark");
+      const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if (prefersDark) {
+        root.classList.add("dark");
+      } else {
+        root.classList.remove("dark");
+      }
+      localStorage.setItem("theme", "system");
     }
+    setTheme(theme);
   }
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="bg-light-buttonBg dark:bg-dark-buttonBg text-white dark:text-black border-light-buttonBorder dark:border-dark-buttonBorder p-2 rounded"
-    >
-      Toggle Theme
-    </button>
+    <div className="flex space-x-2">
+      <Button onClick={() => applyTheme("system")} variant={theme === "system" ? "default" : "outline"}>
+        <Monitor className="h-5 w-5" />
+      </Button>
+      <Button onClick={() => applyTheme("light")} variant={theme === "light" ? "default" : "outline"}>
+        <Sun className="h-5 w-5" />
+      </Button>
+      <Button onClick={() => applyTheme("dark")} variant={theme === "dark" ? "default" : "outline"}>
+        <Moon className="h-5 w-5" />
+      </Button>
+    </div>
   );
 }
